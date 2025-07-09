ac8e12275307597a9e8fa796222b893eb866c191
Nile Kim <nile.coderiver@gmail.com>
2025-07-09 13:23:17 +0900

    added basler pylon dll as reference

--- BulbPicker.App/MainWindow.xaml.cs
+++ BulbPicker.App/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BulbPicker.App
 {
@@ -23,37 +15,14 @@ namespace BulbPicker.App
 
         private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
             if (e.ClickCount == 2)
             {
-                // 더블클릭 시 최대화/복원
                 WindowState = WindowState == WindowState.Maximized
                     ? WindowState.Normal
                     : WindowState.Maximized;
             }
-            else
-            {
-                // 드래그로 윈도우 이동
-                this.DragMove();
-            }
+            else DragMove();
         }
-
-        // 버튼 이벤트들도 추가
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void Maximize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState == WindowState.Maximized
-                ? WindowState.Normal
-                : WindowState.Maximized;
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+        private void Close_Click(object sender, RoutedEventArgs e) => Close();
     }
 }

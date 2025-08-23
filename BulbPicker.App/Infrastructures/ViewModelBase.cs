c328149ddad6751fd68b640c60f306778e66d11e
Nile Kim <nile.coderiver@gmail.com>
2025-08-23 16:31:39 +0900

    Implement ViewModelBase

--- BulbPicker.App/Infrastructures/ViewModelBase.cs
+++ BulbPicker.App/Infrastructures/ViewModelBase.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace BulbPicker.App.Infrastructures
 {
-    class ViewModelBase
+    class ViewModelBase : INotifyPropertyChanged
     {
-
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
